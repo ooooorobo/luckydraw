bd5e1cf136ec59240cdad3ebf36c941944bb266a
조예진 <orobos654@gmail.com>
2024-12-01 01:23:03 +0900

    add server

---
+++ server/server.js
@@ -0,0 +1,38 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import express from 'express';
+import { createServer as createViteServer } from 'vite';
+import * as fs from 'fs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+async function createServer() {
+  const app = express();
+
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+  });
+
+  app.use(vite.middlewares);
+
+  app.use('*', async (req, res, next) => {
+    const url = req.originalUrl;
+
+    try {
+      let template = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8');
+
+      template = await vite.transformIndexHtml(url, template);
+
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(template);
+    } catch (e) {
+      vite.ssrFixStacktrace(e);
+      next(e);
+    }
+  });
+
+  app.listen(3000);
+  console.log(`App is running on http://localhost:3000`);
+}
+
+createServer();
